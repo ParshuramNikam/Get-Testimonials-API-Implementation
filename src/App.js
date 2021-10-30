97611aa60f0fbc00b774a3c7e428f12e68726c4a
Parshuram Nikam <parshunikam0612@gmail.com>
2021-10-30 20:25:00 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import Testimonial from "./components/Testimonial";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	return (
+		<div className="App h-100 bg-indigo-100">
+			<h1 className="text-4xl text-indigo-500 font-medium title-font text-gray-900 pt-4 mb-2 text-center">
+				Testimonials Data Example
+			</h1>
+			<section className="text-gray-600 body-font lg:w-5/6 md:w-6/6 sm:w-full mx-auto p-2 md:p-4">
+				<div className="flex flex-wrap -mx-2 mb-8 m-3">
+					<Testimonial />
+				</div>
+			</section>
+		</div>
+	);
 }
-
-export default App;
